0d6e251f10bc01a7f3c3d5d8fdc838a4b5cc5020
JeramieLiebenguth <31897998+JeramieLiebenguth@users.noreply.github.com>
2018-01-29 21:35:45 -0500

    Lab02 Project

---
+++ Lab02/Lab02/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab02.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public string Index()
+        {
+            return "Product/Index is displayed";
+        }
+        //Products/Browse
+        public string Browse()
+        {
+            return "Browse displayed";
+        }
+        //Products/Details/105
+        public string Details()
+        {
+            return "Details displayed for Id=105";
+        }
+        //Products/Location?zip=44124
+        public string Location ()
+        {
+            return "Location displayed for zip=44124";
+        }
+    }
+}
