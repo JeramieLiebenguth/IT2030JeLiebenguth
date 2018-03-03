eda7877be91eba30167c795a01e7f02022e06f8f
JeramieLiebenguth <31897998+JeramieLiebenguth@users.noreply.github.com>
2018-03-02 20:46:42 -0500

    part 1

--- MVCMusicStoreApplication/MVCMusicStoreApplication/Models/Album.cs
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Models/Album.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-
+using System.Web.Mvc;
 namespace MVCMusicStoreApplication.Models
 {
+    [Bind(Exclude("AlbumID"))]
+
     public class Album
     {
         public virtual int AlbumId { get; set; }
         public virtual int GenreId { get; set; }
         public virtual int ArtistId { get; set; }
+        [Display(Name = "Album Title")]
+        [Required(ErrorMessage = "Album Title cannot be blank")]
         public virtual string Title { get; set; }
+        [Display(Name = "Album Price")]
+        [Required]
+        [Range(0.01,100.00,ErrorMessage ="Album price must be between $0.01 and $100.00")]
+        [DisplayFormat(DataFormatString ="{0:c")]
         public virtual decimal Price { get; set; }
         public virtual Genre Genre { get; set; }
         public virtual Artist Artist { get; set; }
+        [Display(Name = "Album Art Url")]
         public virtual string AlbumArtUrl {get;set;}
         public virtual string AdditionalInfo { get; set; }
         public bool InStock { get; set; }
